0140a4c0945f92331e48e347c429daed797325e3
kratikayadavv <kratikay68@gmail.com>
2026-01-09 10:08:04 +0530

    Create band_name_generator.py

---
+++ Day_01_Band_Name_Generator/band_name_generator.py
@@ -0,0 +1,5 @@
+#brandnamegenerator
+print("Welcome to this page")
+city=input("which city did you grow up in?")
+pet=input("what is your pet's name?")
+print("the best brandname for you is : " + city +" "+ pet)
